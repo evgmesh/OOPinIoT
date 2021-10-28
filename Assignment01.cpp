e9fffa6229b0b157fe758647271c5ddef74ab433
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2021-10-28 16:57:04 +0300

    First Ass is ready

---
+++ Assignment01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include<cstdlib>
+using namespace std;
+
+int mean(int *array, int n);
+
+int main() {
+    int size, *a;
+    srand(time(NULL));
+    cout << "How many numbers you what to use?" << endl;
+    cin >> size;
+    a = new int [size];
+    for (int i = 0; i<size; i++) {
+        a[i]=rand() % 100;
+        cout << a[i] << " ";
+    }
+    cout << endl;
+    cout << "Mean of this " << size << " numbers is: " << mean(a,size);
+    delete [] a;
+
+}
+int mean(int *array, int n)
+{
+    int sum = 0;
+    for(int i = 0; i<n; i++)
+        sum += array[i];
+    return sum/n;
+}
