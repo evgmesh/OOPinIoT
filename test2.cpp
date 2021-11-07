5e54b1bfa028f254f7469fe89035149634d392e5
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2021-11-07 14:38:25 +0000

    Sun, Nov 7, 2021, 2:38 PM +00:00

--- test2.cpp
+++ test2.cpp
@@ -1,16 +1,34 @@
 #include <iostream>
 #include <vector>
+#include <iomanip>
+#include <cstdlib>
 #include <string>
+#include <sstream>
+
+
 
 using namespace std;
-
-int main()
-{
-    vector<string> msg {"Hello", "C++", "World", "from", "VS Code", "and the C++ extension!"};
-    
-    for (const string& word : msg)
-    {
-        cout << word << " ";
+int main() {
+    string buffer;
+    vector<double> db;
+    vector<double>::iterator i;
+    double dbbuf = 0;
+    cout << "Please enter numbers formate: [1.1]. End with negative number\n";
+    getline(cin, buffer);
+    stringstream (buffer) >> dbbuf;
+    while (dbbuf != -1 && dbbuf != 0) {
+        db.push_back(dbbuf);
+        getline(cin, buffer);
+        stringstream (buffer) >> dbbuf;
     }
-    cout << endl;
+    sort(db.begin(), db.end());
+    for(auto i = db.begin(); i != db.end(); ++i)
+        cout << *i << endl;
+    return 0;
 }
+//
+//    while (!(cin >> buf)) {
+//        cout << "Bad value!\n";
+//        cin.clear();
+//        cin.ignore(numeric_limits<streamsize>::max(), '\n');
+//    }
