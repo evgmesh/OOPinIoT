df1ec2df9343167511db99f073ddff3e91d81a1a
evgeniim <evgenii.meshcheriakov@metropolia.fi>
2021-11-10 08:47:56 +0200

    change in gitignore

---
+++ Assignment04/cmake-build-debug/CMakeFiles/Assignment04.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "Assignment04"
+  "Assignment04.pdb"
+  "CMakeFiles/Assignment04.dir/Time.cpp.o"
+  "CMakeFiles/Assignment04.dir/main.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Assignment04.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
