7dbdb5d6ed466edfc74a08ade3d6accff1872b92
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2021-12-21 08:47:43 +0200

    Memory leaks fixed, two mains

--- Assignment13/Website.cpp
+++ Assignment13/Website.cpp
@@ -3,3 +3,20 @@
 //
 
 #include "Website.h"
+
+Website::Website(const char *n) :
+    name(n ? n : "www.cars" + std::to_string(rand() % 99 + 1) + ".com") {}
+
+void advertise(Car *car) {
+    listing.push_back(car);
+}
+void print(std::ostream &out = std::cout) {
+    out << name << std::endl;
+    for(auto car : listing) {
+        out << *car;
+        out << name << " end of list" << std::endl;
+    }
+}
+void remove(Car *car) {
+    listing.erase(std::remove(listing.begin(), listing.end(), car),listing.end());
+}
--- Assignment13/Website.h
+++ Assignment13/Website.h
@@ -1,19 +1,20 @@
 //
-// Created by Evgenii Meshcheriakov on 12/17/21.
+// Created by Evgeny Meshcheryakov on 18.12.2021.
 //
 
 #ifndef ASSIGNMENT13_WEBSITE_H
 #define ASSIGNMENT13_WEBSITE_H
 
+#include <vector>
 
+#include "Car.h"
 class Website {
 public:
-    Website(const char *n = nullptr) : name(n ? n : "www.cars" + std::to_string(rand() % 99 + 1) + ".com") {}
+    Website(const char *n = nullptr);
     ~Website() { std::cout << name << " deleted" << std::endl; };
-    void advertise(Car *car) { listing.push_back(car); }
-    void print(std::ostream &out = std::cout) { out << name << std::endl; for(auto car : listing) out << *car; out << name << " end of list" << std::endl;
-    }
-    void remove(Car *car) { listing.erase(std::remove(listing.begin(), listing.end(), car),listing.end()); }
+    void advertise(Car *car);
+    void print(std::ostream &out = std::cout);
+    void remove(Car *car);
 private:
     std::vector<Car *> listing;
     std::string name;
