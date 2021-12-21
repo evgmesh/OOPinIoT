3ed435edf56ab2cc3e6e29ca10259cccbc7d9166
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2021-12-21 09:10:18 +0200

    My last code in one file

--- Assignment13/lab13_car_sales_1.cpp
+++ Assignment13/lab13_car_sales_1.cpp
@@ -11,9 +11,9 @@
 #include <ctime>
 
 class Car {
-	friend std::ostream& operator<<(std::ostream& out, const Car& car);
+	friend std::ostream &operator<<(std::ostream &out, const Car &car);
 public:
-	Car(const char* m = "Ford", const char* l = nullptr, int ml = 0) : model(m), license(l ? l : (rand() % 2 ? "FOO-" : "BAR-") + std::to_string(rand() % 999 + 1)), mileage(ml ? ml : rand() % 10000) {}
+	Car(const char* m="Ford", const char *l=nullptr, int ml=0) : model(m), license(l?l:(rand()%2?"FOO-":"BAR-") + std::to_string(rand()%999+1)), mileage(ml?ml:rand()%10000) {}
 	~Car() { std::cout << model << " " << license << " deleted" << std::endl; };
 	void Read();
 	std::string GetLicense() const { return license; }
@@ -32,7 +32,7 @@ void Car::Read()
 	mileage = 10000 + rand() % 10000;
 }
 
-std::ostream& operator<<(std::ostream& out, const Car& car)
+std::ostream & operator<<(std::ostream & out, const Car & car)
 {
 	std::cout << "Model: " << car.model << std::endl << "License: " << car.license << std::endl << "Mileage: " << car.mileage << std::endl;
 
@@ -43,13 +43,12 @@ std::ostream& operator<<(std::ostream& out, const Car& car)
 
 class Website {
 public:
-	Website(const char* n = nullptr) : name(n ? n : "www.cars" + std::to_string(rand() % 99 + 1) + ".com") {}
+	Website(const char *n = nullptr) : name(n ? n : "www.cars" + std::to_string(rand() % 99 + 1) + ".com") {}
 	~Website() { std::cout << name << " deleted" << std::endl; };
 	void advertise(std::shared_ptr<Car> car) {
-		listing.push_back(car);
+		listing.push_back(car); 
 	}
-	void print(std::ostream& out = std::cout) {
-		out << name << std::endl; for (auto car : listing) out << *car; out << name << " end of list" << std::endl;
+	void print(std::ostream &out = std::cout) { out << name << std::endl; for(auto car : listing) out << *car; out << name << " end of list" << std::endl;
 	}
 	//void remove(Car *car) { listing.erase(std::remove(listing.begin(), listing.end(), car),listing.end()); } 
 private:
@@ -58,9 +57,9 @@ private:
 };
 
 class Dealer {
-	friend std::ostream& operator<<(std::ostream& out, const Dealer& dealer);
+	friend std::ostream &operator<<(std::ostream &out, const Dealer &dealer);
 public:
-	Dealer(const char* name_ = "John Doe") : name(name_) {};
+	Dealer(const char *name_ = "John Doe") : name(name_) {};
 	~Dealer() { std::cout << name << " deleted" << std::endl; };
 	void buy();
 	void sell();
@@ -73,7 +72,7 @@ public:
 		}
 	}
 	void add_site(std::shared_ptr<Website> w) {
-		sites.push_back(w);
+		sites.push_back(w); 
 	}
 private:
 	std::string name;
@@ -97,13 +96,13 @@ void Dealer::sell()
 	std::cin >> license;
 	auto ci = std::find_if(cars.begin(), cars.end(), [&license](std::shared_ptr<Car> c) {return license == c->GetLicense(); });
 	if (ci != cars.end()) {
-		//		for (auto site : sites) //site->remove(*ci); // modify code wso that this is not needed
+//		for (auto site : sites) //site->remove(*ci); // modify code wso that this is not needed
 		cars.erase(ci);
 	}
 }
 
 
-std::ostream& operator<<(std::ostream& out, const Dealer& dealer)
+std::ostream & operator<<(std::ostream & out, const Dealer & dealer)
 {
 	std::cout << dealer.name << "'s cars for sale" << std::endl;
 	for (auto car : dealer.cars) std::cout << *car;
@@ -188,7 +187,7 @@ void car_sales()
 
 }
 
-int main(int argc, char** argv) {
+int main(int argc, char **argv) {
 	_CrtMemState s1;
 	_CrtMemCheckpoint(&s1);
 
@@ -196,7 +195,7 @@ int main(int argc, char** argv) {
 
 	car_sales();
 
-	_CrtMemState s2, s3;
+	_CrtMemState s2,s3;
 	_CrtMemCheckpoint(&s2);
 	if (_CrtMemDifference(&s3, &s1, &s2)) {
 		_CrtDumpMemoryLeaks();
