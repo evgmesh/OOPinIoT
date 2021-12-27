b583476f0f4382dd5515b6577c93a3df5bdbc962
evgeniim <evgenii.meshcheriakov@metropolia.fi>
2021-12-27 10:13:22 +0200

    clean up

---
+++ Contacts/Person.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Evgenii Meshcheriakov on 12/27/21.
+//
+
+#include "Person.h"
---
+++ Contacts/Person.h
@@ -0,0 +1,17 @@
+//
+// Created by Evgenii Meshcheriakov on 12/27/21.
+//
+
+#ifndef CONTACTS_PERSON_H
+#define CONTACTS_PERSON_H
+
+
+class Person {
+public:
+
+private:
+
+};
+
+
+#endif //CONTACTS_PERSON_H
