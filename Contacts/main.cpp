62683577ad5bf35f53b0116212bbae011523c903
evgeniim <evgenii.meshcheriakov@metropolia.fi>
2021-12-28 15:46:25 +0200

    File remover added

--- Contacts/main.cpp
+++ Contacts/main.cpp
@@ -1,5 +1,8 @@
 #include <iostream>
 #include "Person.h"
+
+void file_remover();
+
 int main() {
 
     Person p1("Person 1");
@@ -9,5 +12,11 @@ int main() {
     std::cout << "p2 before:" << std::endl;    p2.Print();
 
     std::cout << "p1 after:" << std::endl;     p1.Print();
+
     return 0;
+}
+void file_remover(){
+    char fileName[20];
+    std::cout<<"Enter the Name of File: ";           std::cin>>fileName;
+    !remove(fileName) ? std::cout<<"File Deleted Successfully!" : std::cout<<"Error Occurred!";
 }
