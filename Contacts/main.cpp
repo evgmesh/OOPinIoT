fc82c9f707d30e770bfc87bc94a8050119b58c22
Evgenii Meshcheriakov <evgeny.meshcheryakov@gmail.com>
2021-12-30 20:34:43 +0200

    private functions

--- Contacts/main.cpp
+++ Contacts/main.cpp
@@ -7,14 +7,15 @@ int main() {
 
     Contacts phonebook;
     phonebook.start();
-//    Person p1("Person 1");
-//    std::cout << "p1 before:" << std::endl;      p1.Print();
-//
-//    Person p2("Person 2", "dfsd", "767854", "dfsd", p1);
-//    std::cout << "p2 before:" << std::endl;    p2.Print();
-//
-//    std::cout << "p1 after:" << std::endl;     p1.Print();
+#if 0
+    Person p1("Person 1");
+    std::cout << "p1 before:" << std::endl;      p1.Print();
 
+    Person p2("Person 2", "dfsd", "767854", "dfsd", p1);
+    std::cout << "p2 before:" << std::endl;    p2.Print();
+
+    std::cout << "p1 after:" << std::endl;     p1.Print();
+#endif
 
     return 0;
 }
